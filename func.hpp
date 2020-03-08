03c64ee2614ebfecba811ddf231cc50d12e30fa5
PetterJ123 <petter.johansson@protonmail.com>
2020-03-08 14:23:41 +0100

    first src code commit

---
+++ func.hpp
@@ -0,0 +1,173 @@
+#ifndef FUNC_HPP
+#define FUNC_HPP
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <unistd.h>
+
+void pauseAndClear();
+void shuffleBoard(char *gBoardPtr);
+void renderGameboard(char *gBoardPtr, std::vector<int> &indexV);
+void cardSelect(int row,
+				int col,
+				std::map<int, int> &gbmap,
+				std::vector<int> &indexV,
+				bool *state);
+int intConcat(int n1, int n2);
+void fillMap(std::map<int, int> &boardMap);
+
+void pauseAndClear()
+{
+#if defined(__APPLE__)
+	system("read -n 1 -s -p \"Press any key to continue...\"");
+	system("clear");
+#elif defined(_WIN32)
+	system("pause");
+	system("cls");
+#elif defined(__gnu_linux__)
+	std::cin.get();
+	system("clear");
+#endif
+}
+
+// renderfunktion
+void renderGameboard(char *gBoardPtr, std::vector<int> &indexV)
+{
+	// std::cout << ind1 << " : " << ind2 << "\n";
+	for (int i = 0; i < 20; i++)
+	{
+		if (i % 5 == 0)
+		{
+			std::cout << "\n";
+		}
+
+		if (i == indexV.at(0))
+		{
+			std::cout << gBoardPtr[indexV.at(0)] << " ";
+		}
+		else if (i == indexV.at(1))
+		{
+			std::cout << gBoardPtr[indexV.at(1)] << " ";
+		}
+		else
+		{
+			std::cout << "# ";
+		}
+	}
+}
+
+// shuffle funktion
+void shuffleBoard(char *gBoardPtr)
+{
+	char gameBoard[20];
+	char character = 'a';
+
+	const int amnt = 20;
+	const int maxNum = 20;
+	int randomNumber;
+	int randomNumberArr[amnt];
+	srand((unsigned)time(0));	  // seed for randomization
+	for (int i = 0; i < amnt; i++) // fill array with -1
+		randomNumberArr[i] = -1;
+
+	int index = 0;
+	while (randomNumberArr[amnt - 1] == -1) // loop until array empty of -1
+	{
+		randomNumber = (rand() % maxNum) + 1;
+		bool exists = false;
+		for (int i = 0; i < amnt; i++)
+		{
+			if (randomNumberArr[i] == randomNumber)
+				exists = true;
+		}
+
+		if (!exists) // if random number already not in array, insert random number
+		{
+			randomNumberArr[index] = randomNumber;
+			index++;
+		}
+	}
+
+	// place characters in gameboard
+	for (int i = 0; i < 20; i++)
+	{
+		if (character > 106)
+			character = 97; // insert the doublettes in next iteration
+
+		int ranNum = randomNumberArr[i];
+		gameBoard[ranNum] = character;
+		character++;
+	}
+
+	// create new gameboard to leftshft all elements
+	for (int i = 0; i < 21; i++)
+	{
+		gBoardPtr[i] = gameBoard[i + 1];
+	}
+}
+
+// Concatenates integers
+int intConcat(int n1, int n2)
+{
+	std::string s1 = std::to_string(n1);
+	std::string s2 = std::to_string(n2);
+
+	std::string s3 = s1 + s2;
+
+	int nr = std::stoi(s3);
+
+	return nr;
+}
+
+// fills the map with key values and their respective indexes
+void fillMap(std::map<int, int> &boardMap)
+{
+	int k = 11;
+
+	// match up key to each index in gameboard
+	for (int i = 0; i < 20; i++)
+	{
+		boardMap[k] = i;
+		switch (k)
+		{
+		case 16:
+			k = 21;
+			break;
+		case 26:
+			k = 31;
+			break;
+		case 36:
+			k = 41;
+			break;
+		}
+		k++;
+	}
+}
+
+// function to turn a card around
+void flipCard(int row,
+			  int col,
+			  std::map<int, int> &gbmap,
+			  std::vector<int> &indexV,
+			  bool &h)
+{
+	int key = intConcat(row, col);
+	int index = gbmap.at(key);
+	std::cout << "key: " << key << "; index: " << index << "\n";
+
+	if (h)
+	{
+		indexV[0] = index;
+		h = false;
+	}
+	else
+	{
+		indexV[1] = index;
+		h = true;
+	}
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,104 @@
+#include "func.hpp"
+
+int main()
+{
+	int playerScore1,
+		playerScore2,
+		row,
+		col,
+		counter = 0,
+		*counterPtr = &counter;
+	std::string player1, player2, turn;
+	std::map<int, int> gameboardMap; // Used to map card position to a gameboard index
+	std::vector<int> indexV;
+	indexV.push_back(-1);
+	indexV.push_back(-1);
+	char gameboard[20],
+		*gBoardPtr = gameboard;
+	bool h = true, gameloop = true;
+
+	// maps the board to index
+	fillMap(gameboardMap);
+
+	// shuffle function, fills gameboard with random characters
+	shuffleBoard(gBoardPtr);
+
+	// asking for player names
+	std::cout << "Name of player 1:";
+	std::cin >> player1;
+
+	std::cout << "Name of player 2:";
+	std::cin >> player2;
+
+	// set turn to player1
+	turn = player1;
+
+	std::map<int, int>::iterator it = gameboardMap.begin();
+	int i = 0;
+	while (it != gameboardMap.end())
+	{
+		std::cout << it->first << " : " << it->second << " : " << gameboard[i] << "\n";
+		i++;
+		it++;
+	}
+
+	// gameloop keep game running
+	while (gameloop)
+	{
+		// render gameboard
+		renderGameboard(gBoardPtr, indexV);
+
+		std::cout << "\n"
+				  << turn
+				  << ", first card flip: ";
+		std::cin >> row >> col;
+
+		// flips card
+		flipCard(row,
+				 col,
+				 gameboardMap,
+				 indexV,
+				 h);
+
+		// render gameboard with asked position
+		renderGameboard(gBoardPtr, indexV);
+
+		std::cout << "\n"
+				  << turn
+				  << ", second card flip: ";
+		std::cin >> row >> col;
+
+		// flips the second card
+		flipCard(row,
+				 col,
+				 gameboardMap,
+				 indexV,
+				 h);
+
+		// render gameboard with second asked position
+		renderGameboard(gBoardPtr, indexV);
+
+		// if the characters are alike
+		if (gameboard[indexV[0]] == gameboard[indexV[1]])
+		{
+			std::cout << "\nPOINT FOR " << turn << "\n";
+			pauseAndClear();
+		}
+		else
+		{
+			std::cout << "\nno point\n";
+			pauseAndClear();
+		}
+
+		// changes turn
+		if (turn == player1)
+			turn = player2;
+		else if (turn == player2)
+			turn = player1;
+
+		indexV[0] = -1;
+		indexV[1] = -1;
+	}
+
+	return 0;
+}
